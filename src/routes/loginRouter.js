2ae7ab03b65b6b49a510f8ad5f44c4d7200d0c3c
GuiBrumz <guibrumbatista@gmail.com>
2024-11-11 09:30:10 -0300

    swagger

--- src/routes/loginRouter.js
+++ src/routes/loginRouter.js
@@ -3,6 +3,21 @@ const router = express.Router();
  
 const { login } = require("../controller/loginController")
  
+/**
+ * @swagger
+ * /login:
+ *  post:
+ *    summary: Realiza login de usúario.
+ *    responses:
+ *      200:
+ *        description: Sucesso!
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                type: object
+ */
 router.post('/login', login);
  
 module.exports = router;
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,6 +2,21 @@ const { Router } = require('express');
 const router = Router();
 const { storeUser } = require('../controller/usersController');
  
+/**
+ * @swagger
+ * /user/create:
+ *  post:
+ *    summary: Cadastra o usúario.
+ *    responses:
+ *      200:
+ *        description: Sucesso!
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                type: object
+ */
 router.post('/user/create', storeUser);    
  
 module.exports = router;
